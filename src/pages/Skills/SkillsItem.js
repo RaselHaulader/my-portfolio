1d9d70cc3df35bcf0e03bbf5537fbc138b7b4ccf
Rasel <hauladerrasel2@gmail.com>
2023-02-10 01:24:01 +0600

    project section update

--- src/pages/Skills/SkillsItem.js
+++ src/pages/Skills/SkillsItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const SkillsItem = () => {
+const SkillsItem = (title, percentage,tech) => {
     return (
-        <div>
-            <div className='text-white'>
-                <p>felo</p>
+        <li data-aos="flip-right">
+            <div className="skill-icon"><i className="fa fa-html5" aria-hidden="true"></i></div>
+            <h6 className="skill-title">React js</h6>
+            <h6 className="skill-title1">75%</h6>
+            <div data-aos className="skill-bar skill-bar-react">
             </div>
-        </div>
+        </li>
     );
 };
 
